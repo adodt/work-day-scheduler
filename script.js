a22ee0ff830cf8347db1cdb86ac824d4b9633ac0
Gator <allidodt@gmail.com>
2022-09-16 18:12:36 -0700

    adding comments to JS

---
+++ script.js
@@ -0,0 +1,13 @@
+//Current day display at top
+
+//Timeblocks with business hours
+
+//Color coded for past/present/future
+
+//Click into timeblock to create event
+
+//Save event button
+
+//When save button is clicked, text is saved in local storage
+
+//Events are saved when page is refreshed
