fdd5d094a15001d322943141e4759945c12dc0e1
Gator <allidodt@gmail.com>
2022-09-17 15:37:01 -0700

    save events to local storage

---
+++ README.md
@@ -0,0 +1,36 @@
+
+# Alli Dodt WORK-DAY-SCHEDULER
+
+## Table of Contents
+- [Description](#description)
+- [Link to Website](#website-link)
+- [Github Repository](#github-repo)
+- [Screenshot](#screenshot)
+- [Liscense](#license)
+
+
+#
+#
+### Description
+#### Create a work day scheduler for an 8-hour work day. Current time and day are posted at top of the page. Events are saved when the page is refrshed. 
+
+
+#
+<a id="website-link"></a>
+
+### Link to Website: [Website](https://adodt.github.io/work-day-scheduler/)
+
+<a id="github-repo"></a>
+
+### Github Repository: [password-generator](https://github.com/adodt/work-day-scheduler)
+
+#
+### Screenshot 
+<div align="center">
+<img src="assets/adodt-screenshot.png" width ="500px"/>
+</div>
+
+<a id="license"></a>
+
+#
+### License [MIT](http://choosealicense.com/licenses/mit/)
--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,56 @@
 //Current day display at top
-var today = moment()
+let today = moment()
 $("#currentDay").text(today.format("dddd, MMMM Do YYYY, h:mm:ss a" ));
 
 //Timeblocks with business hours
     //DONE
 
 //Color coded for past/present/future
-    // Check odd, then assign boolean
-if(weekNum % 2) {
-  takeOut = true;
-} else {
-  takeOut = false;
+    //DONE
+    //check time for past, present or future, then assign boolean
+    var checkTime = function (){
+    //current time in military time
+    var currentTime = moment().format('H');
+    var eventAreaElements = $(".eventArea");
+
+    for (var i=0; i < eventAreaElements.length; i++) {
+    //ID as a string
+        var elementID = eventAreaElements[i].id;
+    //grab element by ID to assign new class
+        var manipID = document.getElementById
+        (eventAreaElements[i].id)
+    //assign class based on current time
+        $(eventAreaElements[i].id).removeClass(".past .present .future");
+        if (elementID < currentTime) {
+            $(manipID).addClass("past");
+        } else if (elementID > currentTime) {
+            $(manipID).addClass ("future");
+        } else {
+            $(manipID).addClass("present");
+        }
+    }
 }
-
-$("#4a").text(takeOut + ", because it's currently week " + weekNum); 
-
-
-//Click into timeblock to create event
-
-//Save event button
+//check time every 5min
+setInterval(checkTime(), (1000*60)*5)
 
 //When save button is clicked, text is saved in local storage
+$(".saveBtn").click(function (event){
+    event.preventDefault();
+    var time = $(this).parent().attr("id")
+    var value = $(this).siblings(".eventArea").children(".calendarItem").val();
+    localStorage.setItem(time, value);
+})
+
+console.log(localStorage.getItem("5pm"))
 
 //Events are saved when page is refreshed
+
+$("#5pm .calendarItem").val(localStorage.getItem("9am"))
+$("#5pm .calendarItem").val(localStorage.getItem("10am"))
+$("#5pm .calendarItem").val(localStorage.getItem("11am"))
+$("#5pm .calendarItem").val(localStorage.getItem("12pm"))
+$("#5pm .calendarItem").val(localStorage.getItem("1pm"))
+$("#5pm .calendarItem").val(localStorage.getItem("2pm"))
+$("#5pm .calendarItem").val(localStorage.getItem("3pm"))
+$("#5pm .calendarItem").val(localStorage.getItem("4pm"))
+$("#5pm .calendarItem").val(localStorage.getItem("5pm"))
